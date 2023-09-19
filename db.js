d7b00d9390302a6d17dbbe859263b8ff4766a12c
rithwiks <rseth34@gatech.edu>
2023-09-19 19:21:13 -0400

    Setting up mongoDB connection

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGO_DB_URI = `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.hg4uaq2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const MONGO_DB_URI = `mongodb+srv://rithwikseth:${process.env.DB_PASSWORD}@cluster0.urkygij.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 console.log(MONGO_DB_URI);
 
 module.exports = ()=> {
