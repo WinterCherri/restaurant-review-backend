2271d3562854afa9f9b6dbb63f9c02a4a2b4f227
WinterCherri <decaturhoangyen@gmail.com>
2023-10-24 19:22:51 -0400

    ChatGPT Api endpointset up

--- index.js
+++ index.js
@@ -1,42 +1,97 @@
-const dotenv = require('dotenv');
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import OpenAI from 'openai';
+
+
+
+/*CONFIGURATIONS */
 dotenv.config();
-
-const express = require('express')
-const bodyParser = require('body-parser')
-
-//local imports
-const connectDb = require('./db.js');
 const app = express();
-
-
-const mongoose = require('mongoose');
-
-
-//middleware
-app.use(bodyParser.json());
-
-// handle JSON requests and responses nicely
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
 
-//this is a dummy GET request
-app.get('/dummy', (req, res) => {
-    res.send('Hello World');
+
+/* OPENAI CONFIG */
+
+export const openai = new OpenAI({
+  apiKey: process.env.OPEN_API_KEY // This is also the default, can be omitted
 });
 
-// start the application so that it listens at port 8081
-const port = process?.env?.PORT || 8081;
 
-//http://localhost:8081/dummy
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-});
 
-connectDb()
-    .then(() => {
-        console.log('Successfully connect to MongoDB.');
-    })
-    .catch((err) => {
-        console.error('Connection error', err);
-        process.exit();
-    });
+app.post('/', async (req, res) => {
+    try {
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            max_tokens: 7,
+            messages: [{role: "user", content: req.body.message}],
+        });
+        
+        // Extract the assistant's reply from the OpenAI response
+        const assistantReply = response.choices[0].message.content
+
+        // Send the response back to the client
+        res.json({ message: assistantReply });
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("An error occurred while processing your request.");
+    }
+
+})
+/*SERVER SETUP */
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+    console.log('example app listening at', PORT)
+})
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+// const express = require('express')
+// const bodyParser = require('body-parser')
+
+// //local imports
+// const connectDb = require('./db.js');
+// const app = express();
+
+
+// const mongoose = require('mongoose');
+
+
+// //middleware
+// app.use(bodyParser.json());
+
+// // handle JSON requests and responses nicely
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// //this is a dummy GET request
+// app.get('/dummy', (req, res) => {
+//     res.send('Hello World');
+// });
+
+// // start the application so that it listens at port 8081
+// const port = process?.env?.PORT || 8081;
+
+// //http://localhost:8081/dummy
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// });
+
+// connectDb()
+//     .then(() => {
+//         console.log('Successfully connect to MongoDB.');
+//     })
+//     .catch((err) => {
+//         console.error('Connection error', err);
+//         process.exit();
+//     });
---
+++ routes/openai.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import axios from "axios";
+import dotenv from "dotenv";
+import { openai } from '../index.js';
+
+dotenv.config();
+
+const rounter = express.Router();
+
+rounter.post("/text", async (req, res) => {
+    try {
+        const {text, activeChatId} = req.body;
+        console.log('text', text);
+        res.status(200).json({ text })
+    } catch (error) {
+        console.error("error", error);
+        res.status(500).json({ error: error.message })
+    }
+})
+
+export default router;
